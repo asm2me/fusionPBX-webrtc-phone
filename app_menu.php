e8ba670664bdfe2e0f7c6bba7e252e5db72e49b4
asm2me <49681638+asm2me@users.noreply.github.com>
2026-04-09 06:13:01 +0200

    Remove WebRTC Phone parent menu item
    - Removed WebRTC Phone parent menu item (collapsible parent)
    - Made Phone, Click-to-Dial Setup, and CRM Integration direct children of Advanced
    - All three menu items now display at same level under Advanced parent
    - Simplified menu hierarchy

--- app_menu.php
+++ app_menu.php
@@ -7,21 +7,9 @@
 */
 
 	$y = 0;
-	$apps[$x]['menu'][$y]['title']['en-us'] = "WebRTC Phone";
-	$apps[$x]['menu'][$y]['uuid'] = "e5f6a7b8-c9d0-1234-efab-345678901234";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
-	$apps[$x]['menu'][$y]['category'] = "internal";
-	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-phone";
-	$apps[$x]['menu'][$y]['path'] = "";
-	$apps[$x]['menu'][$y]['order'] = "";
-	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
-	$apps[$x]['menu'][$y]['groups'][] = "admin";
-	$apps[$x]['menu'][$y]['groups'][] = "user";
-	$y++;
-
 	$apps[$x]['menu'][$y]['title']['en-us'] = "Phone";
 	$apps[$x]['menu'][$y]['uuid'] = "e5f6a7b8-c9d0-1234-efab-345678901235";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "e5f6a7b8-c9d0-1234-efab-345678901234"; //webrtc phone
+	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-phone";
 	$apps[$x]['menu'][$y]['path'] = "/app/webrtc_phone/webrtc_phone.php";
@@ -33,7 +21,7 @@
 
 	$apps[$x]['menu'][$y]['title']['en-us'] = "Click-to-Dial Setup";
 	$apps[$x]['menu'][$y]['uuid'] = "f6a7b8c9-d0e1-2345-fabc-456789012345";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "e5f6a7b8-c9d0-1234-efab-345678901234"; //webrtc phone
+	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-computer-mouse";
 	$apps[$x]['menu'][$y]['path'] = "/app/webrtc_phone/click_to_dial/click_to_dial_setup.php";
@@ -43,7 +31,7 @@
 
 	$apps[$x]['menu'][$y]['title']['en-us'] = "CRM Integration";
 	$apps[$x]['menu'][$y]['uuid'] = "a7b8c9d0-e1f2-3456-abcd-567890123457";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "e5f6a7b8-c9d0-1234-efab-345678901234"; //webrtc phone
+	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-handshake";
 	$apps[$x]['menu'][$y]['path'] = "/app/webrtc_phone/crm_settings.php";
