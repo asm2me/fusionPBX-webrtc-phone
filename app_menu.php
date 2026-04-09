06f5f3619fa5b3a3c185866c0ed96f86c18533f4
asm2me <49681638+asm2me@users.noreply.github.com>
2026-04-09 05:55:24 +0200

    Move WebRTC Phone, Click-to-Dial, and CRM Integration to advanced menu
    - Updated WebRTC Phone plugin to use advanced menu parent
    - Moved Click-to-Dial Setup from applications to advanced submenu
    - Moved CRM Integration from applications to advanced submenu
    - All plugins now consolidated under Advanced
    - Parent UUID: 594d99c5-6128-9c88-ca35-4b33392cec0f

--- app_menu.php
+++ app_menu.php
@@ -9,7 +9,7 @@
 	$y = 0;
 	$apps[$x]['menu'][$y]['title']['en-us'] = "WebRTC Phone";
 	$apps[$x]['menu'][$y]['uuid'] = "e5f6a7b8-c9d0-1234-efab-345678901234";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "b4750c3f-2a86-b00d-b7d0-345c14eca286"; //applications menu
+	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-phone";
 	$apps[$x]['menu'][$y]['path'] = "/app/webrtc_phone/webrtc_phone.php";
@@ -21,7 +21,7 @@
 
 	$apps[$x]['menu'][$y]['title']['en-us'] = "Click-to-Dial Setup";
 	$apps[$x]['menu'][$y]['uuid'] = "f6a7b8c9-d0e1-2345-fabc-456789012345";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "b4750c3f-2a86-b00d-b7d0-345c14eca286"; //applications menu
+	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-computer-mouse";
 	$apps[$x]['menu'][$y]['path'] = "/app/webrtc_phone/click_to_dial/click_to_dial_setup.php";
@@ -32,7 +32,7 @@
 
 	$apps[$x]['menu'][$y]['title']['en-us'] = "CRM Integration";
 	$apps[$x]['menu'][$y]['uuid'] = "a7b8c9d0-e1f2-3456-abcd-567890123457";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "b4750c3f-2a86-b00d-b7d0-345c14eca286"; //applications menu
+	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f"; //advanced
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['icon'] = "fa-solid fa-handshake";
 	$apps[$x]['menu'][$y]['path'] = "/app/webrtc_phone/crm_settings.php";
